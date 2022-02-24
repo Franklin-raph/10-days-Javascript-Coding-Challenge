64872bb0b66032b67c671b42d2f956f7e0b78943
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-02-24 17:43:40 +0100

    Completed todas challenge of printing the multiplication table between 1 to 12

--- printEvenNums.js
+++ printEvenNums.js
@@ -2,3 +2,13 @@
 for(let i = 0; i<=10; i+=2){
     console.log(i)
 }
+
+// Print the multiplication table between 1 to 10
+let res = '\n'
+let nums = [1,2,3,4,5,6,7,8,9,10,11,12]
+nums.forEach((i) => {
+    for(let j = 1; j < 13; j++){
+        res += j*i + '  '
+    }
+    console.log(res += '\n')
+});
